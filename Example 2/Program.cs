f22aaba7a6388a215ae1b06e0fb160b27067b7d0
CmdWilson <141258229+CmdWilson@users.noreply.github.com>
2023-09-19 20:07:49 +0300

    Add files via upload

---
+++ Example 2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write("Введите значение n=");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите значение m=");
+int m = int.Parse(Console.ReadLine());
+int max=0;
+int min=0;
+int result = 0;
+if (n>m)
+{
+  max=n;
+  min=m;
+}
+else
+{
+  max=m;
+  min=n;
+}
+while (min < max + 1)
+if (min > 0)
+{
+  result = result + min;
+  min++;
+}
+else
+{
+  min++;
+}
+Console.Write(result);
